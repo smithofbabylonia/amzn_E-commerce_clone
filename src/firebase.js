42d12baee052cc6ed3ddc13eac8be1c485590555
Martin Malaza <63983567+smithofbabylonia@users.noreply.github.com>
2025-01-16 18:58:15 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDWpJcEu_8m93I1y46z5cLu-wurtoJjZIc",
-    authDomain: "clone-95cb4.firebaseapp.com",
-    projectId: "clone-95cb4",
-    storageBucket: "clone-95cb4.appspot.com",
-    messagingSenderId: "125836286493",
-    appId: "1:125836286493:web:976510408c9a024960c4f4",
-    measurementId: "G-D821FY1XX8"
-  };
+const firebaseConfig = {};
 
   const app = firebase.initializeApp(firebaseConfig);
   const db = app.firestore();
